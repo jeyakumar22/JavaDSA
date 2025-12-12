061be38ff99d6ce937284978fce91921a52a2caa
Jeyakumar_M <126266932+jeyakumar22@users.noreply.github.com>
2025-12-12 17:32:36 +0530

    Add files via upload

--- String/ReverseWords.java
+++ String/ReverseWords.java
@@ -6,15 +6,15 @@ public class ReverseWords {
         // Testcase 1
         String s1 = "the sky is blue";
         String s2 = "  hello   world  ";
-        System.out.println(reverseWords(s1));
+        System.out.println(reverseWords1(s1));
 
         // Testcase 2
         //String s2 = "  hello   world  ";
-        System.out.println(reverseWords(s2));
+        System.out.println(reverseWords1(s2));
 
         // Testcase 3
         String s3 = "a good   example";
-        System.out.println(reverseWords(s3));
+        System.out.println(reverseWords1(s3));
     }
 
     // 👉 Write ONLY your logic here (no extra printing)
@@ -44,6 +44,26 @@ public class ReverseWords {
             else{
                 ans=temp;
             }
+        }
+        return ans;
+    }public static String reverseWords1(String s) {
+        String temp ="";
+        String ans ="";
+        int i=s.length()-1;
+        while(i>=0){
+            while(i>=0 && s.charAt(i) == ' ')i--;
+            if(i<0) break;
+
+            int end = i;
+            while(i>=0 && s.charAt(i) !=' '){
+                i--;
+            }
+            int start =i+1;
+            //temp = s.substring(start,end+1);
+            //System.out.println(temp);
+            if(!ans.isEmpty()) ans += " " + s.substring(start,end+1);
+            else ans += s.substring(start,end+1);
+            //System.out.println(ans);
         }
         return ans;
     }
